422afd797b653b6b2c5f933b30a447eaab1610b6
Leung Chun Ho <horstleung@gmail.com>
2014-10-07 17:33:29 +0800

    Init Upload

---
+++ HorizontalCollectionViewLayout.h
@@ -0,0 +1,13 @@
+//
+//  HorizontalCollectionViewLayout.h
+//  HK Search
+//
+//  Created by Horst Leung on 3/10/14.
+//  Copyright (c) 2014年 VDELEGATE Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HorizontalCollectionViewLayout : UICollectionViewLayout
+@property (nonatomic, assign) CGSize itemSize;
+@end
